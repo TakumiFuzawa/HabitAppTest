5bb818470e293993ecfedeb16a5e98f1f049dbc4
Fuzawa <futaku0811@gmail.com>
2020-12-20 16:54:36 +0900

    first commit

---
+++ HabitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HabitApp
+//
+//  Created by Takumi Fuzawa on 2020/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
